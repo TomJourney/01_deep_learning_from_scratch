e04d9017bdc00d502c0c395dd60f27941285ad8e
TomJourney <pacosongreen@gmail.com>
2025-09-08 19:05:59 +0800

    compare step func and sigmoid.

---
+++ chapter03/test3_2_5_compare.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# 3.2.5 sigmoid函数和阶跃函数的比较
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# sigmoid 画图
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # 指定y轴范围
+
+# 阶跃函数画图
+def step_function(x):
+    return np.array(x>0, dtype=int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y, linestyle='--')
+plt.ylim(-0.1, 1.1) # 指定y轴的范围
+plt.show()
+
