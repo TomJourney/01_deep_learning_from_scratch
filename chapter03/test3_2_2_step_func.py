e3e407286b4a4ccd5d7035d9608641b7618bfef7
TomJourney <pacosongreen@gmail.com>
2025-09-08 18:46:32 +0800

    step func and img and sigmoid func.

---
+++ chapter03/test3_2_2_step_func.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+# 阶跃函数
+def step_function(x):
+    y = x > 0
+    return y.astype(int)
+
+
+x = np.array([-1.0, 1.0, 2.0])
+print(x)  # [-1.  1.  2.]
+y = x > 0
+print(y)  # [False  True  True]
+
+# 布尔类型转为int型
+print(y.astype(int)) # [0 1 1]
+
+
---
+++ chapter03/test3_2_3_img_step_func.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+# 3.2.3 阶跃函数的图形
+def step_function(x):
+    return np.array(x>0, dtype=int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # 指定y轴的范围
+plt.show()
+
+
+
+
